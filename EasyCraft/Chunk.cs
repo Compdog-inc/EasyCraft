baee7dda003baed3aab8f31c5ee766b36fb4cafa
Compdog Inc <rabbitkrolik.v@gmail.com>
2021-08-04 23:08:18 +0300

    Format file

--- EasyCraft/Chunk.cs
+++ EasyCraft/Chunk.cs
@@ -6,217 +6,217 @@ using System.Threading.Tasks;
 
 namespace EasyCraft
 {
-	public class Chunk
-	{
-		public ChunkCoord coord;
+    public class Chunk
+    {
+        public ChunkCoord coord;
 
-		MeshRenderer chunkRenderer;
-		int vertexIndex = 0;
-		List<Vector3> vertices = new List<Vector3>();
-		List<Color4> colors = new List<Color4>();
-		List<int> triangles = new List<int>();
-		List<Vector2> uvs = new List<Vector2>();
-		List<float> ids = new List<float>();
+        MeshRenderer chunkRenderer;
+        int vertexIndex = 0;
+        List<Vector3> vertices = new List<Vector3>();
+        List<Color4> colors = new List<Color4>();
+        List<int> triangles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<float> ids = new List<float>();
 
-		int[] voxelMap;
-		int mapWidth;
-		int mapHeight;
+        int[] voxelMap;
+        int mapWidth;
+        int mapHeight;
 
-		public bool active
+        public bool active
         {
-			get => chunkRenderer.active;
-			set => chunkRenderer.active = value;
+            get => chunkRenderer.active;
+            set => chunkRenderer.active = value;
         }
 
-		public Vector3 position
-		{
-			get { return chunkRenderer.transform.position; }
-		}
-
-		public void Destroy()
+        public Vector3 position
         {
-			chunkRenderer.Destroy();
+            get { return chunkRenderer.transform.position; }
         }
 
-		public Chunk(ChunkCoord _coord)
+        public void Destroy()
         {
-			mapWidth = StaticData.ChunkWidth;
-			mapHeight = StaticData.ChunkHeight;
-			voxelMap = new int[mapWidth * mapHeight * mapWidth];
+            chunkRenderer.Destroy();
+        }
 
-			coord = _coord;
-			chunkRenderer = new MeshRenderer() { name = "Chunk_" + coord.ToString("{0}_{1}") };
-			chunkRenderer.triangleSize = TriangleSize.UInt;
-			chunkRenderer.transform.position = new Vector3(coord.x * mapWidth, 0f, coord.z * mapWidth);
-			chunkRenderer.material = new Material(World.Instance.ChunkMaterial);
+        public Chunk(ChunkCoord _coord)
+        {
+            mapWidth = StaticData.ChunkWidth;
+            mapHeight = StaticData.ChunkHeight;
+            voxelMap = new int[mapWidth * mapHeight * mapWidth];
 
-			PopulateVoxelMap();
-			CreateMeshData();
-			CreateMesh();
-		}
+            coord = _coord;
+            chunkRenderer = new MeshRenderer() { name = "Chunk_" + coord.ToString("{0}_{1}") };
+            chunkRenderer.triangleSize = TriangleSize.UInt;
+            chunkRenderer.transform.position = new Vector3(coord.x * mapWidth, 0f, coord.z * mapWidth);
+            chunkRenderer.material = new Material(World.Instance.ChunkMaterial);
 
-		bool IsVoxelInChunk(int x, int y, int z)
-		{
-			return !(x < 0 || x > mapWidth - 1 || y < 0 || y > mapHeight - 1 || z < 0 || z > mapWidth - 1);
-		}
+            PopulateVoxelMap();
+            CreateMeshData();
+            CreateMesh();
+        }
 
-		void PopulateVoxelMap()
-		{
-			for (int i = 0; i < voxelMap.Length; i++)
-			{
-				voxelMap[i] = World.Instance.GetVoxel(Mathf.GetVector3FromIndex(i, mapWidth, mapHeight) + position);
-			}
-		}
+        bool IsVoxelInChunk(int x, int y, int z)
+        {
+            return !(x < 0 || x > mapWidth - 1 || y < 0 || y > mapHeight - 1 || z < 0 || z > mapWidth - 1);
+        }
 
-		void CreateMeshData()
-		{
-			ClearMeshData();
-			for (int i = 0; i < voxelMap.Length; i++)
-			{
-				if (World.Instance.GetBlockType(voxelMap[i]).solid)
-					AddVoxelDataToChunk(Mathf.GetVector3FromIndex(i, mapWidth, mapHeight));
-			}
-		}
+        void PopulateVoxelMap()
+        {
+            for (int i = 0; i < voxelMap.Length; i++)
+            {
+                voxelMap[i] = World.Instance.GetVoxel(Mathf.GetVector3FromIndex(i, mapWidth, mapHeight) + position);
+            }
+        }
 
-		public int GetVoxel(int index)
-		{
-			return voxelMap[index];
-		}
+        void CreateMeshData()
+        {
+            ClearMeshData();
+            for (int i = 0; i < voxelMap.Length; i++)
+            {
+                if (World.Instance.GetBlockType(voxelMap[i]).solid)
+                    AddVoxelDataToChunk(Mathf.GetVector3FromIndex(i, mapWidth, mapHeight));
+            }
+        }
 
-		public int GetVoxelR(Vector3 pos)
-		{
-			int index = Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight);
-			if (index < voxelMap.Length && index >= 0)
-				return voxelMap[Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight)];
-			return 0;
-		}
+        public int GetVoxel(int index)
+        {
+            return voxelMap[index];
+        }
 
-		public int GetVoxelR(float x, float y, float z)
-		{
-			return GetVoxelR(new Vector3(x, y, z));
-		}
+        public int GetVoxelR(Vector3 pos)
+        {
+            int index = Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight);
+            if (index < voxelMap.Length && index >= 0)
+                return voxelMap[Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight)];
+            return 0;
+        }
 
-		public int GetVoxel(Vector3 pos)
-		{
-			pos -= position;
-			int index = Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight);
-			if(index < voxelMap.Length && index >= 0)
-				return voxelMap[Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight)];
-			return 0;
-		}
+        public int GetVoxelR(float x, float y, float z)
+        {
+            return GetVoxelR(new Vector3(x, y, z));
+        }
 
-		public int GetVoxel(float x, float y, float z)
-		{
-			return GetVoxel(new Vector3(x, y, z));
-		}
+        public int GetVoxel(Vector3 pos)
+        {
+            pos -= position;
+            int index = Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight);
+            if (index < voxelMap.Length && index >= 0)
+                return voxelMap[Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight)];
+            return 0;
+        }
 
-		bool CheckVoxel(Vector3 pos)
-		{
-			int x = Mathf.FloorToInt(pos.X);
-			int y = Mathf.FloorToInt(pos.Y);
-			int z = Mathf.FloorToInt(pos.Z);
+        public int GetVoxel(float x, float y, float z)
+        {
+            return GetVoxel(new Vector3(x, y, z));
+        }
 
-			if (!IsVoxelInChunk(x, y, z))
-				return World.Instance.GetBlockType(World.Instance.GetVoxel(pos + position)).solid;
+        bool CheckVoxel(Vector3 pos)
+        {
+            int x = Mathf.FloorToInt(pos.X);
+            int y = Mathf.FloorToInt(pos.Y);
+            int z = Mathf.FloorToInt(pos.Z);
 
-			return World.Instance.GetBlockType(voxelMap[Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight)]).solid;
-		}
+            if (!IsVoxelInChunk(x, y, z))
+                return World.Instance.GetBlockType(World.Instance.GetVoxel(pos + position)).solid;
 
-		void AddVoxelDataToChunk(Vector3 pos)
-		{
+            return World.Instance.GetBlockType(voxelMap[Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight)]).solid;
+        }
+
+        void AddVoxelDataToChunk(Vector3 pos)
+        {
             Color4 aointensity = new Color4(0.3f)
             {
                 Alpha = 1f
             };
 
-			for (int p = 0; p < 6; p++)
-			{
-				if (!CheckVoxel(pos + StaticData.faceChecks[p]))
-				{
-					int id = voxelMap[Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight)];
+            for (int p = 0; p < 6; p++)
+            {
+                if (!CheckVoxel(pos + StaticData.faceChecks[p]))
+                {
+                    int id = voxelMap[Mathf.GetIndexFromVector3(pos, mapWidth, mapHeight)];
 
-					vertices.Add(pos + StaticData.voxelVerts[StaticData.voxelTris[p, 0]]);
-					vertices.Add(pos + StaticData.voxelVerts[StaticData.voxelTris[p, 1]]);
-					vertices.Add(pos + StaticData.voxelVerts[StaticData.voxelTris[p, 2]]);
-					vertices.Add(pos + StaticData.voxelVerts[StaticData.voxelTris[p, 3]]);
+                    vertices.Add(pos + StaticData.voxelVerts[StaticData.voxelTris[p, 0]]);
+                    vertices.Add(pos + StaticData.voxelVerts[StaticData.voxelTris[p, 1]]);
+                    vertices.Add(pos + StaticData.voxelVerts[StaticData.voxelTris[p, 2]]);
+                    vertices.Add(pos + StaticData.voxelVerts[StaticData.voxelTris[p, 3]]);
 
-					AddAO(pos, p, aointensity);
-					uvs.AddRange(StaticData.voxelUvs);
-					AddTexture(World.Instance.GetBlockType(id).GetTextureID(p));
+                    AddAO(pos, p, aointensity);
+                    uvs.AddRange(StaticData.voxelUvs);
+                    AddTexture(World.Instance.GetBlockType(id).GetTextureID(p));
 
-					triangles.Add(vertexIndex);
-					triangles.Add(vertexIndex + 1);
-					triangles.Add(vertexIndex + 2);
-					triangles.Add(vertexIndex + 2);
-					triangles.Add(vertexIndex + 1);
-					triangles.Add(vertexIndex + 3);
-					vertexIndex += 4;
-				}
-			}
-		}
-
-		void AddAO(Vector3 pos, int p, Color4 aointensity)
-        {
-			Color4[] aocolors = new Color4[4] { Color.White, Color.White, Color.White, Color.White };
-			if (StaticData.faceChecks[p].Y > 0)
-			{
-				for (int ac = 0; ac < StaticData.aoChecks.Length; ac++)
-				{
-					if (CheckVoxel(pos + StaticData.aoChecks[ac] + Vector3.Up))
-					{
-						if (StaticData.aoIndices[p, 0] > -1)
-							aocolors[StaticData.aoIndices[ac, 0]] -= aointensity;
-						if (StaticData.aoIndices[ac, 1] > -1)
-							aocolors[StaticData.aoIndices[ac, 1]] -= aointensity;
-					}
-				}
-			}
-			else if (StaticData.faceChecks[p].Y == 0)
-			{
-				if (CheckVoxel(pos + StaticData.faceChecks[p] + Vector3.Down))
-				{
-					aocolors[0] -= aointensity * 2;
-					aocolors[2] -= aointensity * 2;
-				}
-			}
-			colors.AddRange(aocolors);
-		}
-
-		void AddTexture(int id)
-		{
-			ids.Add(id);
-			ids.Add(id);
-			ids.Add(id);
-			ids.Add(id);
-		}
-
-		void ClearMeshData()
-        {
-			vertices.Clear();
-			triangles.Clear();
-			colors.Clear();
-			uvs.Clear();
-			ids.Clear();
-			vertexIndex = 0;
+                    triangles.Add(vertexIndex);
+                    triangles.Add(vertexIndex + 1);
+                    triangles.Add(vertexIndex + 2);
+                    triangles.Add(vertexIndex + 2);
+                    triangles.Add(vertexIndex + 1);
+                    triangles.Add(vertexIndex + 3);
+                    vertexIndex += 4;
+                }
+            }
         }
 
-		void CreateMesh()
-		{
-			Mesh mesh = new Mesh();
-			mesh.vertices = vertices.ToArray();
-			mesh.triangles = triangles.ToArray();
-			mesh.colors = colors.ToArray();
-			mesh.uv = uvs.ToArray();
-			mesh.id = ids.ToArray();
-
-			chunkRenderer.mesh = mesh;
-		}
-
-		public void ReloadRender()
+        void AddAO(Vector3 pos, int p, Color4 aointensity)
         {
-			chunkRenderer.material.Dispose();
-			chunkRenderer.material = new Material(World.Instance.ChunkMaterial);
+            Color4[] aocolors = new Color4[4] { Color.White, Color.White, Color.White, Color.White };
+            if (StaticData.faceChecks[p].Y > 0)
+            {
+                for (int ac = 0; ac < StaticData.aoChecks.Length; ac++)
+                {
+                    if (CheckVoxel(pos + StaticData.aoChecks[ac] + Vector3.Up))
+                    {
+                        if (StaticData.aoIndices[p, 0] > -1)
+                            aocolors[StaticData.aoIndices[ac, 0]] -= aointensity;
+                        if (StaticData.aoIndices[ac, 1] > -1)
+                            aocolors[StaticData.aoIndices[ac, 1]] -= aointensity;
+                    }
+                }
+            }
+            else if (StaticData.faceChecks[p].Y == 0)
+            {
+                if (CheckVoxel(pos + StaticData.faceChecks[p] + Vector3.Down))
+                {
+                    aocolors[0] -= aointensity * 2;
+                    aocolors[2] -= aointensity * 2;
+                }
+            }
+            colors.AddRange(aocolors);
+        }
 
-		}
-	}
+        void AddTexture(int id)
+        {
+            ids.Add(id);
+            ids.Add(id);
+            ids.Add(id);
+            ids.Add(id);
+        }
+
+        void ClearMeshData()
+        {
+            vertices.Clear();
+            triangles.Clear();
+            colors.Clear();
+            uvs.Clear();
+            ids.Clear();
+            vertexIndex = 0;
+        }
+
+        void CreateMesh()
+        {
+            Mesh mesh = new Mesh();
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.colors = colors.ToArray();
+            mesh.uv = uvs.ToArray();
+            mesh.id = ids.ToArray();
+
+            chunkRenderer.mesh = mesh;
+        }
+
+        public void ReloadRender()
+        {
+            chunkRenderer.material.Dispose();
+            chunkRenderer.material = new Material(World.Instance.ChunkMaterial);
+
+        }
+    }
 }
